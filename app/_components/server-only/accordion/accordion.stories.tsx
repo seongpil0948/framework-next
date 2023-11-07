bfa774884632531d25701a4d6e3a9d7d33a27ccf
jason0814 <jason0814@iabacus.co.kr>
2023-11-07 15:24:58 +0900

    Feat: add accordion storybook ui

---
+++ app/_components/server-only/accordion/accordion.stories.tsx
@@ -0,0 +1,75 @@
+import { Meta } from "@storybook/react";
+import { CmAccordion } from ".";
+import { HeadphonesIcon } from "@nextui-org/shared-icons";
+
+export default {
+  title: "Components/Accordion",
+  component: CmAccordion,
+  parameters: {
+    layout: "centered",
+    nextjs: {
+      appDirectory: true,
+      navigation: {
+        pathname: "/profile",
+        query: {
+          user: "santa",
+        },
+      },
+    },
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    variant: {
+      control: { type: "select" },
+    },
+    selectionMode: {
+      control: { type: "select" },
+    },
+    selectionBehavior: {
+      control: { type: "select" },
+    },
+  },
+} satisfies Meta<typeof CmAccordion>;
+
+export const Default = {
+  args: {
+    accordionList: [
+      {
+        id: 1,
+        ariaLabel: 'default',
+        title: 'default',
+        contents: 'contents',
+      },
+      {
+        id: 2,
+        ariaLabel: 'use subtitle',
+        title: 'use subtitle',
+        subtitle: 'subtitle',
+        contents: 'contents2',
+      },
+      {
+        id: 3,
+        ariaLabel: 'disabled',
+        title: 'is disabled',
+        contents: '',
+        disabled: true,
+      },
+      {
+        id: 4,
+        ariaLabel: 'custom',
+        title: <span className="text-secondary">ist's custom title</span>,
+        subtitle: <span className="text-primary">ist's custom style</span>,
+        indicator: <HeadphonesIcon className="w-5 h-5" />,
+        disableIndicatorAnimation: true,
+        contents: <div>hi it's <strong>custom style</strong></div>,
+      },
+      {
+        id: 5,
+        ariaLabel: 'use icon',
+        title: 'use icon',
+        startContent: <HeadphonesIcon className="w-5 h-5" />,
+        contents: "it's content",
+      },
+    ]
+  },
+};
