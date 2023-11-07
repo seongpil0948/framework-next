01d6ffd07f378fe587fbdce7b67ff49efa2fbf2a
지은 <hanzsver@gmail.com>
2023-11-07 16:21:18 +0900

    fix: build error

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -13,7 +13,11 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
       >
         <span className="inline-flex items-center text-blue-500">
           {props.children}
-          <svg aria-hidden="true" fill="none" focusable="false" height="1em" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="1em" class="flex mx-1 text-current self-center"><path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path><path d="M15 3h6v6"></path><path d="M10 14L21 3"></path></svg>
+          <svg aria-hidden="true" fill="none" focusable="false" height="1em" shape-rendering="geometricPrecision" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" width="1em">
+            <path d="M18 13v6a2 2 0 01-2 2H5a2 2 0 01-2-2V8a2 2 0 012-2h6"></path>
+            <path d="M15 3h6v6"></path>
+            <path d="M10 14L21 3"></path>
+          </svg>
         </span>
       </a>
     ),
