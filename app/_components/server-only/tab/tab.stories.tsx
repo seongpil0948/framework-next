caa353f6f1e20ddfcaf5e6d1e6c009b01245a92d
jason0814 <jason0814@iabacus.co.kr>
2023-11-07 16:34:42 +0900

    Feat: add tab storybook ui

---
+++ app/_components/server-only/tab/tab.stories.tsx
@@ -0,0 +1,64 @@
+import { Meta } from "@storybook/react";
+import { CmTab } from ".";
+import { HeadphonesIcon } from "@nextui-org/shared-icons";
+
+export default {
+  title: "Components/Tab",
+  component: CmTab,
+  parameters: {
+    layout: "centered",
+    nextjs: {
+      appDirectory: true,
+      navigation: {
+        pathname: "/profile",
+        query: {
+          user: "santa",
+        },
+      },
+    },
+  },
+  tags: ["autodocs"],
+  argTypes: {
+    size: {
+      control: { type: "select" },
+    },
+    variant: {
+      control: { type: "select" },
+    },
+    radius: {
+      control: { type: "select" },
+    },
+    // selectionMode: {
+    //   control: { type: "select" },
+    // },
+    // selectionBehavior: {
+    //   control: { type: "select" },
+    // },
+  },
+} satisfies Meta<typeof CmTab>;
+
+export const Default = {
+  args: {
+    tabs: [
+      {
+        id: 1,
+        ariaLabel: 'tab1',
+        title: 'tab1',
+        contents: 'contents',
+      },
+      {
+        id: 2,
+        ariaLabel: 'tab2',
+        title: 'tab2',
+        contents: 'contents2',
+      },
+      {
+        id: 3,
+        ariaLabel: 'disabled',
+        title: 'is disabled',
+        contents: '',
+        disabled: true,
+      },
+    ]
+  },
+};
