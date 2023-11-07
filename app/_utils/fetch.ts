5bb128b6bdfb32f1590a61631d4d9432e58a4077
최성필 <choeseongpil@choeseongpils-MacBook-Air.local>
2023-11-07 15:25:39 +0900

    for test

--- app/_utils/fetch.ts
+++ app/_utils/fetch.ts
@@ -11,13 +11,13 @@ export async function fetcher(input: RequestInfo, init?: RequestInit) {
   let url = isStr ? input : input.url;
   console.log("[fetching] input url: ", url);
 
-  if (
-    url.startsWith(process.env.NEXT_PUBLIC_BACKEND_BASE_PATH) &&
-    process.env.NEXT_PUBLIC_ACF_ENV !== "local"
-  ) {
-    url = process.env.NEXT_PUBLIC_BACKEND_URL + url;
-    console.log("[fetching] backend url: ", url);
-  }
+  // if (
+  //   url.startsWith(process.env.NEXT_PUBLIC_BACKEND_BASE_PATH) &&
+  //   process.env.NEXT_PUBLIC_ACF_ENV !== "local"
+  // ) {
+  //   url = process.env.NEXT_PUBLIC_BACKEND_URL + url;
+  //   console.log("[fetching] backend url: ", url);
+  // }
   return fetch(isStr ? url : { ...input, url }, {
     ...init,
     credentials: "include",
