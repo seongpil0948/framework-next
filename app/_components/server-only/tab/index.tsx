bcbd017932308038f847725264d870758610deab
jun0108 <96496496+jun0108@users.noreply.github.com>
2023-11-07 16:46:36 +0900

    Feat: add tab storybook ui (#39)

---
+++ app/_components/server-only/tab/index.tsx
@@ -0,0 +1,50 @@
+import React, { ReactNode } from 'react';
+import {Tabs, Tab} from "@nextui-org/tabs";
+
+interface TabProps {
+  tabs: [
+    {
+      id: string;
+      ariaLabel?: string;
+      title: string;
+      contents: string;
+      disabled?: boolean
+    }
+  ];
+  size?: 'sm' | 'md' | 'lg';
+  variant?: 'solid' | 'bordered' | 'light' | 'underlined';
+  color?: 'default' | 'primary' | 'secondary' |'success' | 'warning' | 'danger';
+  radius?: 'none' | 'sm' | 'md' | 'lg'| 'full';
+  fullWidth?: boolean
+}
+
+export const CmTab = ({
+  tabs,
+  size = 'md',
+  variant = 'solid',
+  color = 'default',
+  radius = 'md',
+  fullWidth,
+}: TabProps) => {
+
+  return (
+    <Tabs
+      size={size}
+      variant={variant}
+      color={color}
+      radius={radius}
+      fullWidth={fullWidth}
+    >
+      {tabs.map((tab) => (
+        <Tab 
+          key={`tab-key-${tab.id}`}
+          aria-label={tab.ariaLabel}
+          title={tab.title}
+          isDisabled={tab.disabled}
+        >
+          {tab.contents}
+        </Tab>
+        ))}
+    </Tabs>
+  );
+};
