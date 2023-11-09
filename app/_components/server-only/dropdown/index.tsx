d83de7bdec27833c91a2db4a03fa181fd5068065
cro3u <148185835+cro3u@users.noreply.github.com>
2023-11-09 23:46:29 +0900

    Feat: dropdown storybook ui (#51)
    Feat: select chip 추가, CmChip 내부 close btn
    
    Refactor: dropdown props
    
    Feat: use button onPress in dropdown
    
    Co-authored-by: simdaa <simdaa>

---
+++ app/_components/server-only/dropdown/index.tsx
@@ -0,0 +1,115 @@
+import React, { useState } from 'react'
+import { Select, SelectItem } from '@nextui-org/select'
+import { CmChip } from '../chip'
+import { CmButton } from '../button'
+interface DropdownItem {
+  item?: string
+  value?: string
+  label?: string
+  startContent?: React.ReactNode
+}
+interface DropdownProps {
+  dropdownItem: DropdownItem[]
+  selectionMode?: 'single' | 'multiple'
+  triggerType?: 'input' | 'button'
+  selectedKeys?: 'all' | React.Key[]
+  disabledKeys?: 'all' | React.Key[]
+  defaultSelectedKeys?: 'all' | React.Key[]
+  size?: 'sm' | 'md' | 'lg'
+  placeholder?: string
+  label?: string
+  labelPlacement?: 'inside' | 'outside' | 'outside-left'
+  required?: boolean
+  disabled?: boolean
+  readOnly?: boolean
+  selectorIcon?: React.ReactNode
+  isInvalid?: boolean
+  validationState?: 'valid' | 'invalid'
+  errorMsg?: string
+  successMsg?: string
+  useChip?: boolean
+  isOpen?: boolean
+  onOpenChange?: (isOpen: boolean) => void
+}
+
+export const CmDropdown = ({
+  dropdownItem,
+  triggerType = 'input',
+  selectionMode = 'single',
+  selectedKeys,
+  disabledKeys,
+  defaultSelectedKeys,
+  size = 'md',
+  placeholder = '선택해주세요.',
+  label,
+  labelPlacement = 'outside',
+  required = false,
+  disabled = false,
+  readOnly = false,
+  selectorIcon,
+  isInvalid = false,
+  validationState,
+  errorMsg = 'errorMessage',
+  successMsg = 'successMessage',
+  useChip = false,
+  ...props
+}: DropdownProps) => {
+  const [isOpen, setIsOpen] = React.useState(false)
+  const dropdownBtn = () => (
+    <CmButton onPress={() => setIsOpen(!isOpen)}>
+      {isOpen ? 'Close' : 'Open'}
+    </CmButton>
+  )
+
+  return (
+    <>
+      <div className="flex items-end">
+        <Select
+          isOpen={isOpen}
+          onOpenChange={(open) => open !== isOpen && setIsOpen(open)}
+          items={dropdownItem}
+          selectionMode={selectionMode}
+          disabledKeys={disabledKeys}
+          selectedKeys={selectedKeys}
+          defaultSelectedKeys={defaultSelectedKeys}
+          size={size}
+          placeholder={placeholder}
+          label={label}
+          labelPlacement={labelPlacement}
+          isRequired={required}
+          isDisabled={disabled}
+          selectorIcon={selectorIcon}
+          isInvalid={isInvalid === true || validationState === 'invalid'}
+          errorMessage={
+            (isInvalid === true || validationState === 'invalid') && errorMsg
+          }
+          renderValue={
+            useChip
+              ? (items) => {
+                  return (
+                    <div className="flex gap-2">
+                      {items.map((item: any) => (
+                        <CmChip key={item.value} content={item.data.label} />
+                      ))}
+                    </div>
+                  )
+                }
+              : undefined
+          }
+        >
+          {(item) => (
+            <SelectItem
+              key={`dropdown-item-key-${item.value}`}
+              textValue={item.value}
+              startContent={item.startContent}
+            >
+              {item.label}
+            </SelectItem>
+          )}
+        </Select>
+        {triggerType === 'button' && dropdownBtn()}
+      </div>
+      {validationState === 'valid' && <p>{successMsg}</p>}
+    </>
+  )
+}
