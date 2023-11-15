82ba299be733253f2af5d62866d342ed1bb820b9
seongpil0948 <seongpil0948@gmail.com>
2023-11-16 00:49:41 +0900

    fix: deploy error

--- next.config.js
+++ next.config.js
@@ -5,18 +5,18 @@ const nextConfig = {
   pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
   reactStrictMode: false,
   cleanDistDir: true,
-  async rewrites() {
-    return [
-      {
-        source: '/dsi/api/:path*',
-        destination: 'https://dev.a-stro.sktelecom.com/dsi/api/:path*',
-      },
-      {
-        source: '/acf/api/:path*',
-        destination: 'https://acf.abacussw.co.kr/acf/api/:path*',
-      },
-    ]
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/dsi/api/:path*',
+  //       destination: 'https://dev.a-stro.sktelecom.com/dsi/api/:path*',
+  //     },
+  //     {
+  //       source: '/acf/api/:path*',
+  //       destination: 'https://acf.abacussw.co.kr/acf/api/:path*',
+  //     },
+  //   ]
+  // },
 }
 
 module.exports = withMDX(nextConfig)
