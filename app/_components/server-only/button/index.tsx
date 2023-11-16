d6cd17384c093453dd57ca20dee24fcfd8cbd1f0
jason0814 <jason0814@iabacus.co.kr>
2023-11-16 13:28:38 +0900

    Fix: CmButton interface type

--- app/_components/server-only/button/index.tsx
+++ app/_components/server-only/button/index.tsx
@@ -1,60 +1,61 @@
-import React from 'react'
-import { Button } from '@nextui-org/button'
+import React, { forwardRef } from 'react'
+import { Button, ButtonProps } from '@nextui-org/button'
 
-interface ButtonProps {
-  children: string
-  className?: string
-  variant?:
-    | 'solid'
-    | 'bordered'
-    | 'light'
-    | 'flat'
-    | 'faded'
-    | 'shadow'
-    | 'ghost'
-  color?: 'default' | 'primary' | 'secondary' | 'success' | 'warning' | 'danger'
-  size?: 'sm' | 'md' | 'lg'
-  radius?: 'none' | 'sm' | 'md' | 'lg' | 'full'
-  fullWidth?: boolean
-  startContent?: string
-  endContent?: string
-  isIconOnly?: boolean
-  disabled?: boolean
-  onPress?: () => void
-  onClick?: () => void
+interface CmButtonProps extends ButtonProps {
+  ref?: ButtonProps['ref']
+  className?: ButtonProps['className']
+  children?: ButtonProps['children']
+  variant?: ButtonProps['variant']
+  color?: ButtonProps['color']
+  size?: ButtonProps['size']
+  radius?: ButtonProps['radius']
+  fullWidth?: ButtonProps['fullWidth']
+  startContent?: ButtonProps['startContent']
+  endContent?: ButtonProps['endContent']
+  isIconOnly?: ButtonProps['isIconOnly']
+  disabled?: ButtonProps['disabled']
+  onPress?: ButtonProps['onPress']
 }
 
-export const CmButton = ({
-  children,
-  className,
-  variant = 'solid',
-  color = 'default',
-  size = 'md',
-  radius = 'md',
-  fullWidth = false,
-  startContent,
-  endContent,
-  isIconOnly = false,
-  disabled = false,
-  onPress,
-  onClick,
-}: ButtonProps) => {
-  return (
-    <Button
-      className={className}
-      variant={variant}
-      color={color}
-      size={size}
-      radius={radius}
-      fullWidth={fullWidth}
-      startContent={startContent}
-      endContent={endContent}
-      isIconOnly={isIconOnly}
-      isDisabled={disabled}
-      onPress={onPress}
-      onClick={onClick}
-    >
-      {children}
-    </Button>
-  )
-}
+export const CmButton = forwardRef<HTMLButtonElement, CmButtonProps>(
+  (
+    {
+      children,
+      className,
+      variant = 'solid',
+      color = 'default',
+      size = 'md',
+      radius = 'md',
+      fullWidth = false,
+      isIconOnly = false,
+      disabled = false,
+      startContent,
+      endContent,
+      onPress,
+      ...props
+    }: CmButtonProps,
+    ref?: React.ForwardedRef<HTMLButtonElement>,
+  ) => {
+    return (
+      <Button
+        ref={ref}
+        className={className}
+        variant={variant}
+        color={color}
+        size={size}
+        radius={radius}
+        fullWidth={fullWidth}
+        startContent={startContent}
+        endContent={endContent}
+        isIconOnly={isIconOnly}
+        isDisabled={disabled}
+        onPress={onPress}
+        {...props}
+      >
+        {children}
+      </Button>
+    )
+  },
+)
+
+CmButton.displayName = 'CmButton'
