fbabfa72eb2580f439bb056f82ff69e7c772c284
최성필 <choeseongpil@choeseongpils-MacBook-Air.local>
2023-11-17 10:58:27 +0900

    enhanced: add common code

--- app/[lang]/code/page.tsx
+++ app/[lang]/code/page.tsx
@@ -1,11 +1,26 @@
 import { title } from '@/app/_components/server-only/primitives'
-import GroupCodeView from './_components/GroupCodeView'
+import { AVAIL_LOCALES, TAvailLocale } from '@/config/system'
+import { getDictionary } from '../dictionaries'
+import { LoadingSuspense } from '@/app/_components/server-only/suspense'
+import CodeController from './_components/client-only/code-controller'
 
-export default function SSGPage() {
+export async function generateStaticParams() {
+  return AVAIL_LOCALES.map((lang) => ({ lang }))
+}
+
+interface Param {
+  params: { lang: TAvailLocale }
+}
+
+export default async function SSGPage({ params: { lang } }: Param) {
+  const dict = await getDictionary(lang)
   return (
     <div>
-      <h1 className={title()}>Common Code</h1>
-      <GroupCodeView />
+      <h1 className={title()}>{dict['code']['title']}</h1>
+      <p>{typeof window === 'undefined' ? 'server' : 'client'} component</p>
+      <LoadingSuspense>
+        <CodeController />
+      </LoadingSuspense>
     </div>
   )
 }
